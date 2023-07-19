681fcb4a1ceb55323dd7b5ace715e250aea9e1d2
Diógenes Falcade <77336996+diogenesfalcade@users.noreply.github.com>
2023-07-19 16:46:25 -0300

    Update temp_table_select_into.sql
    Add comments

--- temp_table_select_into.sql
+++ temp_table_select_into.sql
@@ -1,3 +1,6 @@
+--  Reason: User wants a button to propagate the information of the sFirstTextId to all the other samples
+--Informations are stored in a global temporary table #MISC_XNI_REQUESTS
+
 SELECT '{sTextId}' AS sample_id, assay, request, quality
 INTO #temp_intermediaria
 FROM ##MISC_XNI_REQUESTS
